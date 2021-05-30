62bbc69c43fd33c9f701b69ac9866e2a15946541
HemangMaan <hemangmaan@gmail.com>
2021-05-30 18:39:35 +0530

    Project Completed

---
+++ Assets/DataFiles/Scripts/ResetPosition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+
+    Vector3 originalPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPosition = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("AeroplaneWall"))
+        {
+            transform.localPosition = originalPosition;
+        }
+        else if (other.gameObject.CompareTag("CloudsWall"))
+        {
+            transform.localPosition = originalPosition;
+        }
+    }
+}
